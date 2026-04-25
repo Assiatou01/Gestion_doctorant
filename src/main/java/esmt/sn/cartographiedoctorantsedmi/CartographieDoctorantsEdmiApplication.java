2a0674a5b936d170af55a868d874a2b41530a694
Assiatou01 <assisalahbah@gmail.com>
2026-04-25 18:57:13 +0000

    Initialisation du projet Backend

---
+++ src/main/java/esmt/sn/cartographiedoctorantsedmi/CartographieDoctorantsEdmiApplication.java
@@ -0,0 +1,13 @@
+package esmt.sn.cartographiedoctorantsedmi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartographieDoctorantsEdmiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CartographieDoctorantsEdmiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/esmt/sn/cartographiedoctorantsedmi/CartographieDoctorantsEdmiApplicationTests.java
@@ -0,0 +1,13 @@
+package esmt.sn.cartographiedoctorantsedmi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartographieDoctorantsEdmiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
